ec5ed047e5830a31b668dcfdaa85570b7f296eef
mshayanabbasi <mshayanabbasi98@gmail.com>
2018-10-30 12:02:37 +0500

    client

---
+++ client.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import grpc
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='World'))
+    print(response.message)
+
+    
+if __name__ == '__main__':
+    run()
